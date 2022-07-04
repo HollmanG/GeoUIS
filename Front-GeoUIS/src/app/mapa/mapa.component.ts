b46681d5c66824bd9658407353429928da6ae71e
HollmanG <estebangonzalez05@hotmail.com>
2022-07-04 14:27:28 -0500

    searchbox

--- Front-GeoUIS/src/app/mapa/mapa.component.ts
+++ Front-GeoUIS/src/app/mapa/mapa.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+//Openlayers
 import { View, Map, Overlay, Feature } from 'ol';
-import LayerSwitcher, { BaseLayerOptions, GroupLayerOptions } from 'ol-layerswitcher';
 import { Point } from 'ol/geom';
 import LayerGroup from 'ol/layer/Group';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat, transform } from 'ol/proj';
 import OSM from 'ol/source/OSM';
 import VectorSource from 'ol/source/Vector';
 import {Circle, Fill, Stroke, Style} from 'ol/style';
+import * as olEasing from 'ol/easing';
+
+//LayerSwitcher
+import LayerSwitcher, { BaseLayerOptions, GroupLayerOptions } from 'ol-layerswitcher';
+
+//ol-ext
+// @ts-ignore
+import Search from "ol-ext/control/Search" ; 
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
+  styleUrls: ['./mapa.component.css', 
+  '../../../node_modules/ol-ext/dist/ol-ext.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MapaComponent implements OnInit {
 
@@ -118,6 +130,49 @@ export class MapaComponent implements OnInit {
           
           console.info(feature.getProperties());
       }
+    });
+
+    // A list of positions to search in
+    const positions = 
+    [	{ name:"Paris", pos:transform([2.351828, 48.856578], 'EPSG:4326', 'EPSG:3857'), zoom:11 },
+      { name:"London", pos:transform([-0.1275,51.507222], 'EPSG:4326', 'EPSG:3857'), zoom:11 },
+      { name:"Geneve", pos:transform([6.149985,46.200013], 'EPSG:4326', 'EPSG:3857'), zoom:13 },
+      { name:"Bruxelles", pos:transform([4.35,50.83], 'EPSG:4326', 'EPSG:3857'), zoom:12 },
+      { name:"Berlin", pos:transform([13.383333,52.516667], 'EPSG:4326', 'EPSG:3857'), zoom:12 },
+      { name:"Madrid", pos:transform([-3.683333,40.433333], 'EPSG:4326', 'EPSG:3857'), zoom:12 },
+      { name:"Roma", pos:transform([12.48657,41.888732], 'EPSG:4326', 'EPSG:3857'), zoom:12 }
+    ]
+
+    // The search control
+	  const search = new Search(
+		{	//target: $(".options").get(0),
+			// Title to use in the list
+			getTitle: function(f: any) { return f.name; },
+			// Search result
+			autocomplete: function (s: any)
+			{	var result = [];
+				// New search RegExp
+				var	rex = new RegExp(s.replace("*","")||"\.*", "i");
+				for (var i=0; i<positions.length; i++)
+				{	if (rex.test(positions[i].name)) result.push (positions[i]);
+				}
+				/* Return result directly... */
+				return result;
+				/* ...or use the callback function
+				cback(result);
+				return false;
+				*/
+			}
+		});
+    this.map.addControl(search);
+
+    // Center when click on the reference index
+    search.on('select', (e: any) =>
+    {	this.map!.getView().animate({
+        center: e.search.pos,
+        zoom: 6,
+        easing: olEasing.easeOut
+      })
     });
 
   }
