f1a56c5c98e8f24465c193e9c97ed1dd7b7bf1b8
HollmanG <estebangonzalez05@hotmail.com>
2022-07-26 21:51:47 -0500

    maqueta login-register

--- Front-GeoUIS/src/app/auth/auth.module.ts
+++ Front-GeoUIS/src/app/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MainAuthComponent } from './pages/main-auth/main-auth.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MainAuthComponent
   ],
   imports: [
     CommonModule,
