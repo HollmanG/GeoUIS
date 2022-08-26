0272877f53a735810079cf52f34079e28bc247bf
Jorge Burgos <burgosjorge2907@gmail.com>
2022-08-25 23:38:25 -0500

    guard login

--- Front-GeoUIS/src/app/auth/auth-routing.module.ts
+++ Front-GeoUIS/src/app/auth/auth-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MainAuthComponent } from './pages/main-auth/main-auth.component';
+import { ValidarUsuarioLoginGuard } from './guards/validar-usuario-login.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MainAuthComponent,
     children: [
-      {path: 'login', component: LoginComponent},
-      {path: 'register', component: RegisterComponent},
+      {path: 'login', component: LoginComponent, canActivate: [ValidarUsuarioLoginGuard] },
+      {path: 'register', component: RegisterComponent , canActivate: [ValidarUsuarioLoginGuard]},
       {path: '**', redirectTo: 'login'},
     ]
   },
---
+++ Front-GeoUIS/src/app/auth/guards/validar-usuario-login.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarUsuarioLoginGuard implements CanActivate, CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(): Observable<boolean> | boolean {
+    if(localStorage.getItem('token') == undefined){
+        return true;
+    }else{
+        this.router.navigateByUrl('/inicio');
+        return false;
+    }
+  }
+
+  canLoad(): Observable<boolean> | boolean {
+    if(localStorage.getItem('token') == undefined){
+        return true;
+    }else{
+        this.router.navigateByUrl('/inicio');
+        return false;
+    }
+  }
+}
