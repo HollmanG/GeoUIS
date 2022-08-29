1566be10c6d0bf6bff5beffe22ab1d1656871695
Jorge Burgos <burgosjorge2907@gmail.com>
2022-08-28 23:43:55 -0500

    configuracion tscongi.json

--- Front-GeoUIS/src/app/muestras/components/agregar-fotos/agregar-fotos.component.ts
+++ Front-GeoUIS/src/app/muestras/components/agregar-fotos/agregar-fotos.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Muestra } from 'src/app/muestras/interfaces/muestra.interface';
 
@@ -9,8 +10,11 @@ import { Muestra } from 'src/app/muestras/interfaces/muestra.interface';
 })
 export class AgregarFotosComponent implements OnInit {
 
+  public archivos: any = [];
+
   constructor(private dialogRef: MatDialogRef<AgregarFotosComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Muestra) { }
+    @Inject(MAT_DIALOG_DATA) public data: Muestra,
+    private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
 
@@ -19,5 +23,35 @@ export class AgregarFotosComponent implements OnInit {
   cerrar(){
     this.dialogRef.close()
   }
+
+  capturarFile(event:any):any{
+    const archivoCapturado = event.target.files[0];
+    this.archivos.push(archivoCapturado);
+    console.log(event.target.files);
+  }
+
+  extraerbase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          blob: $event,
+          image,
+          base: null
+        });
+      };
+
+    } catch (e) {
+      return null;
+    }
+  })
 
 }
